904693fe5ec40fca4c07df1734eede75f0d31dcc
Afiqu3 <gahossainj@gmail.com>
2025-11-06 17:08:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,307 @@
+const express = require("express");
+const cors = require("cors");
+const admin = require("firebase-admin");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const serviceAccount = require("./smart-deals-firebase-adminKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const logger = (req, res, next) => {
+  console.log("logging information");
+  next();
+};
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+  try {
+    const userInfo = await admin.auth().verifyIdToken(token);
+    req.token_email = userInfo.email;
+    next();
+  } catch {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+};
+
+// const verifyFirebaseToken = async (req, res, next) => {
+//   // console.log("in firebase", req.headers.authorization);
+//   const tokenStr = req.headers.authorization;
+//   if (!tokenStr) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = tokenStr.split(" ")[1];
+//   if (!token) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   try {
+//     const userInfo = await admin.auth().verifyIdToken(token);
+//     req.token_email = userInfo.email;
+//     // console.log(userInfo);
+//     next();
+//   } catch {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+// };
+
+const verifyJWTToken = (req, res, next) => {
+  // console.log(req.headers);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    // console.log(decoded)
+    req.token_email = decoded.email;
+    next();
+  });
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5rkszyx.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const smart_dealsDB = client.db("smart_dealsDB");
+    const productsCollection = smart_dealsDB.collection("products");
+    const bidsCollection = smart_dealsDB.collection("bids");
+    const usersCollection = smart_dealsDB.collection("users");
+
+    // jwt apis
+    // require('crypto').randomBytes(64).toString('hex')
+    app.post("/getToken", async (req, res) => {
+      const loggedUser = req.body;
+      const token = jwt.sign(loggedUser, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token: token });
+    });
+
+    // users
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      const email = req.body.email;
+      const query = { email: email };
+      const alreadyUser = await usersCollection.findOne(query);
+
+      if (alreadyUser) {
+        res.send({ message: "User already exits" });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+    // products
+    app.get("/products", async (req, res) => {
+      //   const projectFields = { title: 1 };
+      //   const cursor = productsCollection
+      //     .find()
+      //     .sort({ price_min: -1 })
+      //     .skip(2)
+      //     .limit(2)
+      //     .project(projectFields);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/latest-products", async (req, res) => {
+      const cursor = productsCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/products", verifyFirebaseToken, async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedProduct,
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // bids
+    app.get("/bids", verifyFirebaseToken, async (req, res) => {
+      // console.log(req.token_email);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+
+      if (req.token_email !== email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    
+    // app.get("/bids", verifyJWTToken, async (req, res) => {
+    //   // console.log(req.token_email);
+    //   const email = req.query.email;
+    //   const query = {};
+    //   if (email) {
+    //     query.buyer_email = email;
+    //   }
+
+    //   if (req.token_email !== email) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   const cursor = bidsCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    // app.get("/bids", logger, verifyFirebaseToken, async (req, res) => {
+    //   console.log(req.token_email);
+    //   const email = req.query.email;
+    //   const query = {};
+    //   if (email) {
+    //     if (email !== req.token_email) {
+    //       return res.status(403).send({ message: "forbidden access" });
+    //     }
+    //     query.buyer_email = email;
+    //   }
+    //   const cursor = bidsCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+
+    app.get("/products/bids/:productId", async (req, res) => {
+      const productId = req.params.productId;
+      const query = { product: productId };
+      const cursor = bidsCollection.find(query).sort({ bid_price: 1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // app.get(
+    //   "/products/bids/:productId",
+    //   verifyFirebaseToken,
+    //   async (req, res) => {
+    //     const productId = req.params.productId;
+    //     const query = { product: productId };
+    //     const cursor = bidsCollection.find(query).sort({ bid_price: 1 });
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+    //   }
+    // );
+
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = await bidsCollection.insertOne(newBid);
+      res.send(result);
+    });
+
+    app.get("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBid = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedBid,
+      };
+      const result = bidsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+// client
+//   .connect()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Server is running on port: ${port}`);
+//     });
+//   })
+//   .catch(console.dir);
